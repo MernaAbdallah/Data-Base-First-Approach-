71f01a8ecd8ec48a7d901af1311b8b80592e6766
Merna Abdallah <91502893+MernaAbdallah@users.noreply.github.com>
2023-09-11 17:02:32 +0300

    Add files via upload

---
+++ DB First/ConsoleApp7/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace ConsoleApp7
+{
+    class program
+    {
+
+            public class Student
+        {
+            public int StudentId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Course
+        {
+            public int CourseId { get; set; }
+            public string CourseName { get; set; }
+        }
+
+        public class SchoolContext : DbContext
+        {
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Course> Courses { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                //MERNA-ABDALLA\SQLEXPRESS
+                optionsBuilder.UseSqlServer(@"Server=MERNA-ABDALLA\SQLEXPRESS;Database=SchoolDB;Trusted_Connection=True; TrustServerCertificate=True;");
+            }
+        }
+
+         static void Main(string[] args)
+        {
+
+        }
+
+    }
+    
+}
+
